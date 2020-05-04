4aaf0d2cf508d710dde656372a52bd9c644822f3
Elizabeth Morales Muñoz <elizabeth.morales@ucsp.edu.pe>
2020-05-04 10:47:55 -0500

    'cambiosEliza'

--- front/src/app/pages/forminvitacion/forminvitacion.component.ts
+++ front/src/app/pages/forminvitacion/forminvitacion.component.ts
@@ -9,26 +9,7 @@ export class ForminvitacionComponent implements OnInit {
 
   constructor() { }
 
-  title = 'FormValidation';
-  mobNumberPattern = "^((\\+51-?)|0)?[0-9]{11}$";
-  isValidFormSubmitted = false;
-  user = new User();
-
-
-  onFormSubmit(form: NgForm) {
-    this.isValidFormSubmitted = false;
-    if (form.invalid) {
-      return;
-    }
-    this.isValidFormSubmitted = true;
-    form.resetForm();
+  ngOnInit() {
   }
+
 }
-
-export class User {
-  mobileNumber?: string;
-}
-
-
-
- 
